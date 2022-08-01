3c28f98b89d07cfe81221d3fcdc91aaa7a9a359a
thomas.schuessler <thomas.schuessler@codecentric.de>
2022-08-01 10:49:08 +0200

    mongo db daten lesen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { MongoClient } from "mongodb";
 
-const Home: NextPage = () => {
+interface ShoppingItem {
+  name: string;
+  id: string;
+  picked: boolean;
+}
+
+interface Props {
+  shoppingItems: ShoppingItem[];
+}
+
+const Home: NextPage<Props> = (props) => {
   return (
     <>
       <Head>
@@ -10,9 +21,35 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <h4>TS Next Site</h4>
+        {props.shoppingItems.map((item) => (
+          <p key={item.id}>
+            {item.name} {item.picked ? <span>J</span> : <span>N</span>}
+          </p>
+        ))}
       </main>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://thomas-cc:dyuICI95mJtp9CXx@cluster0.8ioiysa.mongodb.net/shoppingListDb?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const shoppingListCollection = db.collection("shoppingList");
+
+  const shoppingItems = await shoppingListCollection.find().toArray();
+  await client.close();
+
+  return {
+    props: {
+      shoppingItems: shoppingItems.map((item) => ({
+        name: item.name,
+        picked: item.picked,
+        id: item._id.toString(),
+      })),
+    },
+  };
 };
 
 export default Home;
